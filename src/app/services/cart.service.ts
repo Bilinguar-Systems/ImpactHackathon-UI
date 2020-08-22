173aa2ccdee3fdf8896f160cb20e342c8e78248b
carlosmiguelquintos <carlosmiguelquintos@outlook.com>
2020-08-22 21:58:49 +0800

    checkout api implemented

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,11 +1,22 @@
-import { Injectable, Inject } from '@angular/core';
-import { v4 as uuid } from 'uuid';
-import { Observable, of, merge, Subject, BehaviorSubject } from 'rxjs';
-import { scan, startWith, map, tap, combineLatest, switchMap, skipWhile, shareReplay, debounceTime, publish, refCount, share } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from './../../environments/environment';
+import { AuthenticationService } from './authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  constructor() { }
+  constructor(private http: HttpClient, private auth: AuthenticationService) { }
+
+  checkout(payload) {
+    const httpHeaders: HttpHeaders = new HttpHeaders({
+      'Content-Type':  'multipart/form-data',
+      'Authorization': 'Bearer ' + this.auth.currentUserValue
+    });
+
+    return this.http.post(`${environment.api}/api/cart/project/1`, payload, {headers: httpHeaders});
+  }
 }
